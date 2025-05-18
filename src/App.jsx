f1a501d2bed29f7ed46b87629b49a7eb88b30215
Fredrik Koroma <fekoroma@gmail.com>
2025-05-18 01:31:52 +0100

    Initial commit: CareerPin basic starter app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>CareerPin</h1>
+      <p>Welcome to the beginning of your career journey.</p>
+    </div>
+  );
+}
+
+export default App;
